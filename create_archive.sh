f59624e17179a1b3cfd2565410b763ff2ad3dc06
Andreis0605 <andreis0605@gmail.com>
2024-04-12 16:55:55 +0300

    FINAL VERSION

--- create_archive.sh
+++ create_archive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f README ]; then
+if [ ! -f README.md ]; then
     echo "No README present! Exiting..." >&2
     exit 1
 fi
@@ -12,7 +12,7 @@ fi
 
 rm -rf archive.zip
 sudo make clean
-zip -r archive.zip lib/ include/ *.c Makefile README
+zip -r archive.zip lib/ include/ *.c Makefile README.md
 if [ -f arp_table.txt ]; then
     zip archive.zip arp_table.txt
 fi
